9bf91dec922ba3b326e3a47c5b6f90c3afae25eb
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-08-11 20:44:23 -0600

    ordenes terminado

--- src/main/java/com/test/Test02JAVAEEJDMR/Controllers/OrdenJDMRController.java
+++ src/main/java/com/test/Test02JAVAEEJDMR/Controllers/OrdenJDMRController.java
@@ -12,12 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.test.Test02JAVAEEJDMR.Models.OrdenJDMR;
+import com.test.Test02JAVAEEJDMR.Models.ProductoJDMR;
 import com.test.Test02JAVAEEJDMR.Services.Interfaces.IOrdenJDMRService;
 
 @Controller
@@ -62,6 +64,46 @@ public class OrdenJDMRController {
 
         ordenJDMRService.crearOEditar(ordenJDMR);
         attributes.addFlashAttribute("msg", "Orden creada correctamente");
+        return "redirect:/ordenes/index";
+    }
+
+     @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        OrdenJDMR ordenJDMR = ordenJDMRService.buscarPorId(id).get();
+        model.addAttribute("ordenJDMR", ordenJDMR);
+        return "/ordenes/edit";
+    }
+
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable("id") Long id, Model model) {
+        Optional<OrdenJDMR> ordeOptional = ordenJDMRService.buscarPorId(id);
+        if (ordeOptional.isPresent()) {
+            OrdenJDMR ordenJDMR = ordeOptional.get();
+            model.addAttribute("ordenJDMR", ordenJDMR);
+            return "/ordenes/details"; // Asegúrate de que esta ruta coincide con la estructura de carpetas
+        } else {
+            // Manejo de error si el producto no existe
+            return "redirect:/ordenes/index"; // Redirige a una página de error o lista
+        }
+    }
+
+    @GetMapping("/remove/{id}")
+    public String remove(@PathVariable("id") Long id, Model model) {
+        System.out.println("Remove method called with ID: " + id);
+        Optional<OrdenJDMR> orOptional = ordenJDMRService.buscarPorId(id);
+        if (orOptional.isPresent()) {
+            OrdenJDMR ordenJDMR = orOptional.get();
+            model.addAttribute("ordenJDMR", ordenJDMR);
+            return "/ordenes/delete";
+        } else {
+            return "redirect:/ordenes/index";
+        }
+    }
+
+    @PostMapping("/delete")
+    public String delete(OrdenJDMR ordenJDMR, RedirectAttributes attributes) {
+        ordenJDMRService.eliminarPorId(ordenJDMR.getId());
+        attributes.addFlashAttribute("msg", "Orden eliminada correctamente");
         return "redirect:/ordenes/index";
     }
 }
